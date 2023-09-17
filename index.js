c5259b7452b81f57bed1f3a38137490c2ec1ba4b
aam198 <aam198@psu.edu>
2023-09-17 14:07:36 -0400

    Importing packages into entry point

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
 // Importing the required functions for each command
+import addTask from './commands/addTask.js'
+import deleteTask from './commands/deleteTask.js'
+import readTask from './commands/readTask.js'
+import updateTask from './commands/updateTask.js'
+
+// Importing the Command class from Commander.js library
+import { Command } from 'commander'
+
+// Creating an instance of the Command class
+const program = new Command();
