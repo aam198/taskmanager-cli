8ca56bc7e00c9917f35bec23681aa973abe5c96f
aam198 <aam198@psu.edu>
2023-09-17 14:17:56 -0400

    Setting cli tools information

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// Above is shebang to execute with node.js interpreter 
+
 // Importing the required functions for each command
 import addTask from './commands/addTask.js'
 import deleteTask from './commands/deleteTask.js'
@@ -11,3 +13,37 @@ import { Command } from 'commander'
 
 // Creating an instance of the Command class
 const program = new Command();
+
+// Setting the name and description of the CLI tool
+program
+// Both description and version can be accessed by flags --help & --version
+.name('todo')
+.description('Your terminal task manager!')
+.version('1.0.0')
+
+// Defining the command called 'add'
+program
+.command('add')
+.description('Create a new todo.')
+.action(addTask)
+
+// Defining the command 'read'
+program
+.command('read')
+.description('Reads all the todos.')
+.action(readTask)
+
+// Defining the command for 'update'
+program
+.command('update')
+.description('Updates a todo.')
+.action(updateTask)
+
+// Defining the command for 'delete'
+program
+.command('delete')
+.description('Deletes a todo.')
+.action(deleteTask)
+
+// Parsing the command-line arguments and executing the correstponding action
+program.parse()
